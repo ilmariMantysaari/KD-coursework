f1b7aef434c17fe1e3bf10b6acc6f2032ce29047
Ilmari Mäntysaari <mantysaari.ilmari@gmail.com>
2018-11-15 15:17:50 +0200

    clustering file

---
+++ clustering.py
@@ -0,0 +1,5 @@
+
+
+
+def clustering(param):
+    print(param)
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from tkinter import Tk
+# from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
+from tkinter import *
+import clustering
 
 def main():
+    # window = Tk()
+    # textBox=Text(window, height=2, width=10)
+    # textBox.pack()
+    # buttonCommit=Button(window, height=1, width=10, text="Commit", 
+    #                     command=lambda: retrieve_input())
+    # buttonCommit.pack()
     filename = askopenfilename()
+    
+    clustering.clustering([{"avain": "arvo"}, {"avain": "arvo2"}])
+
     print(filename)
 
-
 main()
+
+
+# clusterien määrä
+# 
