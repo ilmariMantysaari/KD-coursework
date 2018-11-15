b1d9d559dca1e1979e2223b9c75055790b3020ec
WeceW <toni.weckroth@gmail.com>
2018-11-15 18:36:02 +0200

    computing euclidean & manhattan distances

--- clustering.py
+++ clustering.py
@@ -19,35 +19,54 @@ class Clustering():
 
         # TODO:
         # Arbitrarily pick K cases as initial cluster centres
-        cluster_centres = [data[1]]
+        cluster_centres = [data[0], data[1]]
         centres_changed = True
         print(cluster_centres)
         while centres_changed:
             for case in data:
-                for att in case:
-                    print(case[att])
                 # TODO:
                 # Assign each case into a cluster whose centre is closest to the
                 # case in the Euclidean/Manhattan distance sense
+                for centre in cluster_centres:
+                    self.euclideanDist(case, centre)
+                    self.manhattanDist(case, centre)
 
                 # TODO:
                 # Compute for each cluster the mean vector of the points
                 # assigned to the cluster. Use these mean vectors as new
                 # cluster centres.
                 centres_changed = False
-
+        print(cluster_centres)
         print()
         return data
 
-    # Computes the Euclidean distance between two cases
+    #######################################################
+    # Computes the EUCLIDEAN DISTANCE between two cases
+    # https://en.wikipedia.org/wiki/Euclidean_distance#Definition
+    # sqrt(sum(Qi - Pi)^2)
     def euclideanDist(self, case1, case2):
-        # TODO:
-        pass
+        if len(case1) != len(case2):
+            return -1
+        euc = 0;
+        for i, att in enumerate(case1):
+            if i > 0: # i.e. ignoring case names
+                # TODO: Assuming now that all the data is float
+                euc += ((float(case1[att])-float(case2[att]))**2)
+        return euc**(0.5)
 
-    # Computes the Manhattan distance between two cases
+    #######################################################
+    # Computes the MANHATTAN DISTANCE between two cases
+    # https://en.wikipedia.org/wiki/Taxicab_geometry#Formal_definition
+    # absolute value of sum(Qi-Pi)
     def manhattanDist(self, case1, case2):
-        # TODO:
-        pass
+        if len(case1) != len(case2):
+            return -1
+        man = 0;
+        for i, att in enumerate(case1):
+            if i > 0: # i.e. ignoring case names
+                # TODO: Assuming now that all the data is float
+                man += abs(float(case1[att])-float(case2[att]))
+        return man**(0.5)
 
 
 
