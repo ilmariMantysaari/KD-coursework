49046c3c5473aff6570cfd23a82fa42389814b66
Lauri Hursti <larnuji@gmail.com>
2018-11-25 15:45:33 +0200

    Add test using iris data for output

---
+++ outputIrisTest.py
@@ -0,0 +1,36 @@
+import csv
+import clustering
+import glob
+import output
+import os
+import pathlib
+import preprocessing
+import unittest
+
+
+class TestClusterOutput(unittest.TestCase):
+
+    # Clean up iris output files
+    def setup(self):
+        imgFiles = glob.glob('iris.csv_output' + '*.png')
+        imgFiles.append('iris.csv_output_combined.gif')
+        for fileName in imgFiles:
+            if pathlib.Path(fileName).is_file():
+                os.remove(fileName)
+
+    def test_irisDataOutput(self):
+        with open('iris.csv', 'r') as datafile:
+            reader = csv.DictReader(datafile, delimiter=';')
+            data = []
+            for line in reader:
+                data.append(line)
+
+            pre = preprocessing.Preprocessing(data)
+            pre.normalizeData()
+
+            cluster = clustering.Clustering()
+            datasCentersTuple = cluster.kMeans(data, k=3, dist='eucl', centre_method='rand', ignored_keys=['class', 'sepal_length'])
+
+            writer = output.ClusterImageWriter('iris.csv', 'output')
+            writer.writeImages(datasCentersTuple[0], datasCentersTuple[1], 'cluster', 'dist2clu', 'petal_width', 'petal_length')
+            writer.writeGif()
