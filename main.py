2d43c2b9d845b3034188f19a42e97dee7a2a0590
Ilmari Mäntysaari <mantysaari.ilmari@gmail.com>
2018-11-29 15:11:58 +0200

    MAin changes

--- main.py
+++ main.py
@@ -6,46 +6,32 @@ from tkinter import *
 import preprocessing
 import clustering
 import copy
-<<<<<<< HEAD
-import csv
-
-def main():
-    # window = Tk()
-    # textBox=Text(window, height=2, width=10)
-    # textBox.pack()
-    # buttonCommit=Button(window, height=1, width=10, text="Commit", 
-    #                     command=lambda: retrieve_input())
-    # buttonCommit.pack()
-    filename = askopenfilename()
-    with open(filename, 'r') as datafile:
-        reader = csv.DictReader(datafile, delimiter=';')
-        data = []
-        for line in reader:
-            data.append(line)
-
-        pre = preprocessing.Preprocessing(data)
-        pre.removeAttributes(["Att1", "Att3", "For example"])
-        pre.normalizeData()
-
-        cluster = clustering.Clustering()
-        data_kmeans = cluster.kMeans(data, k=3, dist='eucl', centre_method='rand', ignored_keys=['Case', 'Cluster'])
-
-        print(filename)
-
-# if __name__ == "__main()__":
-#     main()
-main()
-
-
-# clusterien määrä
-# 
-=======
 import csv     
 
 class ClusterGUI:
     def __init__(self, master):
         self.master = master
         self.filename = ""
+
+        # distance buttons
+        self.dist = StringVar()
+        Label(master, text = "Select distance function").pack()
+        self.eucl_button = Radiobutton(master, text="Euclidean",
+            variable=self.dist, value= clustering.Clustering.DISTANCE_EUCLIDEAN)
+        self.eucl_button.pack()
+        self.manh_button = Radiobutton(master, text="Manhattan",
+            variable=self.dist, value= clustering.Clustering.DISTANCE_MANHATTAN)
+        self.manh_button.pack()
+
+        # method
+        self.method = StringVar()
+        Label(master, text = "Select method function").pack()
+        self.rand_button = Radiobutton(master, text="Random",
+            variable=self.method, value= clustering.Clustering.METHOD_RANDOM)
+        self.rand_button.pack()
+        self.dist_button = Radiobutton(master, text="Distance",
+            variable=self.method, value= clustering.Clustering.METHOD_DISTANCE)
+        self.dist_button.pack()
 
         self.filename_select = Button(master, text="Select file", command=self.get_filename)
         self.filename_select.pack()
@@ -68,11 +54,26 @@ class ClusterGUI:
             pre.normalizeData()
 
             cluster = clustering.Clustering()
-            data_kmeans = cluster.kMeans(data, k=3, dist='eucl', centre_method='rand', ignored_keys=['Case', 'Cluster'])
+            data_kmeans = cluster.kMeans(data, k=3, dist=self.dist, centre_method=self.method, ignored_keys=['Case', 'Cluster'])
 
             print(self.filename)
+
+            # piirrä clusteri
+            print(data_kmeans)
 
 root = Tk()
 gui = ClusterGUI(root)
 root.mainloop()
->>>>>>> 9c1f0f1faa9e9ebed190f76de069dbfff767f19d
+
+# valinnat clusteringin parametreille
+    # checkboxit vivuille
+    # näytä tiedoston nimi
+    # k:n valinta
+# mahd. valinta poistetaville attribuuteille
+# clustereiden näyttö
+    # clusteri graafi
+# clustereista tietoa
+    # määrä
+    # caset/clusteri
+    # centroidit
+
